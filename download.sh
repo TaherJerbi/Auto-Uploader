94d2c2a8dee274a9cf14a7dee7d470538920d210
Debian <azureuser@mydebian.2wt434zurcdubmcwedqamvteya.ax.internal.cloudapp.net>
2021-03-13 20:29:20 +0000

    shell scripts

---
+++ download.sh
@@ -0,0 +1,5 @@
+while IFS='\n' read -r LINE || [ -n "${LINE}" ]; do
+   wget --cookies=on --load-cookies cookies.txt --keep-session-cookies --no-check-certificate -m ${LINE} 
+done < $1
+
+find . -name "*.mp4" > videos
