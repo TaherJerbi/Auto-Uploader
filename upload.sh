92ea14783326c919337fb666526a2ffba338fbe0
TaherJerbi <jerbi.taherr@gmail.com>
2021-05-03 17:36:43 +0100

    upload to Youtube folder

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 
 while IFS='' read -r LINE || [ -n "${LINE}" ]; do
-	/home/azureuser/.google-drive-upload/bin/gupload "${LINE}" -c "Youtube GL2"
+	/home/azureuser/.google-drive-upload/bin/gupload "${LINE}" -c "Youtube"
 done < $1
